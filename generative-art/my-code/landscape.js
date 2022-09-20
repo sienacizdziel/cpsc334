2541df6cc2a8fb0ce3610176b6d9c70425c52609
Siena <siena.cizdziel@yale.edu>
2022-09-19 21:02:03 -0400

    changes

--- generative-art/my-code/landscape.js
+++ generative-art/my-code/landscape.js
@@ -6,44 +6,79 @@ let blocks = []
 
 // parameters
 let t = 0;
-let fr = 20;
+// let fr = 60;
 let numWaves = 3;
 const waveValues = new Array(Math.floor(screenHeight / numWaves));
 const oceanColors = ["#074a53", "#077395", "#5b9299", "#8dc1cf", "#aadbe1", "#b8bec0"];
+const sandColors = ["#f6d7b0", "#f2d2a9", "#eccca2", "#e7c496", "#e1bf92"]
+const sandValues = new Array(20);
+let sand = null;
+const numUmbrellas = 40;
+let umbrellaColors = new Array(numUmbrellas).fill(null);
+let umbrellaLocations = new Array(numUmbrellas).fill(null);
 
 const waveColors = [];
 let theta = 0;
 
 function setup() {
   createCanvas(screenWidth, screenHeight);
-  background(oceanColors[0]);
   noFill();
-  frameRate(fr);
+  // frameRate(fr);
   dx = (TWO_PI / 500.0) * 3; 
   for (let i = blocks[5]; i < blocks[5] + 2 * screenWidth / numScreens; i += 20) {
     waveColors.add(oceanColors[round(random(oceanColors.length - 1))]);
   }
+  sand = sandColors[floor(random(5))];
+  background(sand);
 }
 
 function draw() {
   stroke('black');
   strokeWeight(1);
-  background(51);
+  fill(sand);
+  background(sand);
   createScreenSplits();
-  calcWave();
-  renderWave(blocks[1]);
 
-  strokeWeight(20);
-  for (let i = blocks[5]; i < blocks[5] + 2 * screenWidth / numScreens; i += 20) {
-    if (t % 5 == 0) {
-      waveColors[i - blocks[5]] = oceanColors[round(random(oceanColors.length - 1))];
+  for (let block = 0; block < 2; block += 1) {
+    for (let i = blocks[block] + 300; i < blocks[block] + 750; i += 100) {
+      strokeWeight(20);
+      if (t % 10 == 0) {
+        waveColors[i - blocks[block]] = oceanColors[round(random(oceanColors.length - 1))];
+      }
+      stroke(waveColors[i - blocks[block]]);
+      calcWave();
+      renderWave(i);
+      coverBlock(block + 1);
     }
-    stroke(waveColors[i - blocks[5]]);
-    calcWave();
-    renderWave(i);
   }
 
-  frameRate(fr);
+  for (let i = 0; i < umbrellaColors.length; i++) {
+    if (umbrellaColors[i] == null) {
+      umbrellaColors[i] = color(floor(random(255)), floor(random(255)), floor(random(255)));
+    }
+
+    if (umbrellaLocations[i] == null) {
+      umbrellaLocations[i] = [random(1), random(1)];
+    }
+    fill(umbrellaColors[i]);
+    strokeWeight(0);
+
+    push();
+    translate(width * umbrellaLocations[i][0], height * umbrellaLocations[i][1]);
+    rotate(frameCount / 50.0);
+    polygon(0, 0, 50, 8);
+    pop();
+
+    stroke("black");
+    strokeWeight(10);
+
+    push();
+    translate(width * umbrellaLocations[i][0], height * umbrellaLocations[i][1]);
+    ellipse(0, 0, 1, 1);
+    pop();
+  }
+
+  // frameRate(fr);
   t++;
 }
 
@@ -69,6 +104,51 @@ function rotate() {
 
 function renderWave(translateX) {
   for (let x = 0; x < waveValues.length; x++) {
-    ellipse(translateX + waveValues[x], x * numWaves, 16, 16);
+    ellipse(translateX + waveValues[x], x * numWaves, 40, 40);
   }
+
+  // for (let x = 0; x < waveValues.length; x++) {
+  //   ellipse(translateX * 2 + waveValues[x], x * numWaves, 40, 40);
+  // }
+}
+
+class Umbrella {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = null;
+  }
+
+  create() {
+    console.log(this.x);
+    console.log(this.y);
+    this.color = color(floor(random(255)), floor(random(255)), floor(random(255)));
+  }
+
+  update() {
+    strokeWeight(0);
+    stroke(this.color);
+    fill(this.color);
+
+    strokeWeight(10);
+    stroke('black');
+    ellipse(this.x + 20, this.y + 20, 50, 50);
+  }
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function coverBlock(block) {
+  strokeWeight(0);
+  fill(sand);
+  rect(blocks[block], 0, screenWidth / numScreens, screenHeight);
 }

ab9cb81237fca24ba3b13b575e4bc5b15a11d9de
Siena Cizdziel <55004000+sienacizdziel@users.noreply.github.com>
2022-09-26 20:33:37 -0400

    add comments

--- interactive-devices/task1.py
+++ interactive-devices/task1.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
+# gpio input/output pins
 switchPin = 17
 buttonPin = 23
 joystickPinX = 26
 joystickPinY = 16
 ledPin1 = 18
-ledPin1State = GPIO.LOW
 ledPin2 = 6
+
+# mode 1 = spst switch off
+ledPin1State = GPIO.LOW
+
+# mode 2 = spst switch on
 ledPin2State = GPIO.LOW
 
 def button_callback(channel):
@@ -15,7 +20,7 @@ def button_callback(channel):
     global ledPin1State
     global ledPin2State
 
-    # change state of led 1 in state 1 (switch open)
+    # when in mode 1, change led 1 on button click
     if not GPIO.input(switchPin):
         if ledPin1State == GPIO.LOW:
             ledPin1State = GPIO.HIGH
@@ -23,7 +28,7 @@ def button_callback(channel):
             ledPin1State = GPIO.LOW
         GPIO.output(ledPin1, ledPin1State)
 
-    # change state of led 2 in state 2 (switch closed)
+    # when in mode 2, change led 2 on button click
     else:
         if ledPin2State == GPIO.LOW:
             ledPin2State = GPIO.HIGH
@@ -31,7 +36,7 @@ def button_callback(channel):
             ledPin2State = GPIO.LOW
         GPIO.output(ledPin2, ledPin2State)
 
-
+# gpio setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switchPin, GPIO.IN)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -44,7 +49,7 @@ print("starting...")
 
 GPIO.add_event_detect(buttonPin,GPIO.RISING,callback=button_callback)
 
-# at the outset, light up led 1 if in state 1, and led 2 if in state 2
+# at the outset, light up led 1 if in mode 1, and led 2 if in mode 2
 if not GPIO.input(switchPin):
     ledPin1State = GPIO.HIGH
     ledPin2State = GPIO.LOW
@@ -59,7 +64,9 @@ print(ledPin2State)
 
 try:
     while 1:
+        # listen for when both leds are on (mode 3)
         if ledPin1State == GPIO.HIGH and ledPin2State == GPIO.HIGH:
+            # in mode 3, print continuously (digital )joystick positions
             print("joystick x: " + str(GPIO.input(joystickPinX)))
             print("joystick y: " + str(GPIO.input(joystickPinY)))
         time.sleep(1)
